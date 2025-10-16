2f14acc8b731c5056df942010d875ce3aaec2181
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-10-16 13:52:44 -0400

    implemented dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,6 +5,18 @@ def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
-    
+
     response = requests.post(url, headers=headers, json=input_json)
-    return response.json()
+    emotion_dict = response.json()['emotionPredictions'][0]['emotion']
+    great = great_emotion(emotion_dict)
+    emotion_dict["dominant_emotion"] = great
+    return emotion_dict
+
+def great_emotion(emotions: dict):
+    string = ""
+    num = 0
+    for key in emotions.keys():
+        if num < emotions[key]:
+            num = emotions[key]
+            string = key
+    return string
