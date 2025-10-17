f06a53d740f78f8c51142096ccbf26ddc21a9b7f
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-10-17 16:30:06 -0400

    implemented one test

--- EmotionDetection/test_emotion_detection.py
+++ EmotionDetection/test_emotion_detection.py
@@ -2,4 +2,5 @@ import unittest
 
 from emotion_detection import emotion_detector
 class TestEmotionDetection(unittest.TestCase):
-    
+    def test1(self):
+        self.assertEqual(emotion_detector("I am glad this happened")["dominant_emotion"], "joy")
