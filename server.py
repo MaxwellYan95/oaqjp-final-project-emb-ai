b4165acf28192f9386e495021455c4ddf91f8b74
= <=>
2025-10-18 15:15:22 -0400

    used pylint

--- server.py
+++ server.py
@@ -1,21 +1,28 @@
-from flask import Flask, request, jsonify
+"""
+All import statements
+"""
+from flask import Flask
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
+"""
+Displays the response on the application
+"""
 @app.route('/')
 def index():
-    emotionDict = emotion_detector("")
+    """emotion_dict a dictionary that represents a request"""
+    emotion_dict = emotion_detector("")
     # Format the response as a string similar to the example output in the lab
     response_str = (
         "For the given statement, the system response is "
-        f"'anger': {emotionDict['anger']}, "
-        f"'disgust': {emotionDict['disgust']}, "
-        f"'fear': {emotionDict['fear']}, "
-        f"'joy': {emotionDict['joy']}, "
-        f"'sadness': {emotionDict['sadness']}. "
-        f"The dominant emotion is <b>{emotionDict['dominant_emotion']}</b>."
+        f"'anger': {emotion_dict['anger']}, "
+        f"'disgust': {emotion_dict['disgust']}, "
+        f"'fear': {emotion_dict['fear']}, "
+        f"'joy': {emotion_dict['joy']}, "
+        f"'sadness': {emotion_dict['sadness']}. "
+        f"The dominant emotion is <b>{emotion_dict['dominant_emotion']}</b>."
     )
-    if emotionDict['dominant_emotion'] is None:
+    if emotion_dict['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     return response_str
 
