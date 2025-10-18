09959ca7a113600563ba00eec435233ce522ad48
= <=>
2025-10-18 14:39:55 -0400

    implement error handling (7a)

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
-@app.route('/emotionDetector')
+@app.route('/')
 def index():
     emotionDict = emotion_detector("I think I am having fun")
+    if request.status_code == 400:
+        emotionDict = emptyDict();
     # Format the response as a string similar to the example output in the lab
     response_str = (
         "For the given statement, the system response is "
@@ -16,6 +18,9 @@ def index():
         f"The dominant emotion is <b>{emotionDict['dominant_emotion']}</b>."
     )
     return response_str
+
+def emptyDict():
+    return {'anger': None, 'disgust': None, 'fear': None, 'sadness': None, 'dominant_emotion': None}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
